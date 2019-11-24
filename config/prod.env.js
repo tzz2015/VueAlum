47a50b30626a96d71138cea6294a515bb709fcb5
啊飞_18133 <1374329135@qq.com>
2019-11-24 14:02:43 +0800

    修改端口

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,5 +1,5 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-  API_HOST: '"http://94.191.120.108:8001/api/"',//服务端host
+  API_HOST: '"http://94.191.120.108:2587/api/"',//服务端host
 }
